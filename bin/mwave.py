0b5b93d58c38037c2f386df7b30dfde87d08d18c
Edmundo Carmona <eantoranz@gmail.com>
2009-10-14 18:46:37 -0500

    Wave works as a module

--- bin/mwave.py
+++ bin/mwave.py
@@ -1,36 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright 2009 Edmundo Carmomna
+# Copyright 2009 Edmundo Carmona Antoranz
 # Released under the terms of the Affero GPLv3
 
-import math
+import Wave
 
-class Wave:
-
-    freq = None
-    samplingRate = None
-    maxValue = None
-    binaryOuput = True
-    counter = 0
-
-    def __init__(self, freq, samplingRate = 44100, maxValue = 32000):
-        self.frep = freq
-        self.samplingRate = samplingRate
-        self.maxValue = maxValue
-
-        self.resetCounter()
-
-    def resetCounter(self):
-        self.counter = 0
-
-    def getNextValue(self):
-        temp = math.floor(math.sin(2 * math.pi * self.counter / self.samplingRate) * self.maxValue)
-
-        self.counter+=1
-        return temp
-
-wave = Wave(1)
+wave = Wave.Wave(1)
 i = 0
 while i < 44100:
     print(wave.getNextValue())
