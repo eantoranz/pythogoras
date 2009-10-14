9b525a70bfb65fcb02039c633881b11f40ce3f58
Edmundo Carmona <eantoranz@gmail.com>
2009-10-14 18:53:38 -0500

    Little module reorganization

---
+++ bin/WaveCommon.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2009 Edmundo Carmona Antoranz
+# Released under the terms of the Affero GPLv3
+
+from Wave import Wave
--- bin/mwave.py
+++ bin/mwave.py
@@ -4,9 +4,9 @@
 # Copyright 2009 Edmundo Carmona Antoranz
 # Released under the terms of the Affero GPLv3
 
-import Wave
+from WaveCommon import *
 
-wave = Wave.Wave(1)
+wave = Wave(1)
 i = 0
 while i < 44100:
     print(wave.getNextValue())
