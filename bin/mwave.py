8590b90ae64f9261cb04e1195ab60c5dad23858b
Edmundo Carmona <eantoranz@gmail.com>
2009-10-14 18:36:32 -0500

    Wave calculation (basics) is working

---
+++ bin/mwave.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright 2009 Edmundo Carmomna
+# Released under the terms of the Affero GPLv3
+
+import math
+
+class Wave:
+
+    freq = None
+    samplingRate = None
+    maxValue = None
+    binaryOuput = True
+    counter = 0
+
+    def __init__(self, freq, samplingRate = 44100, maxValue = 32000):
+        self.frep = freq
+        self.samplingRate = samplingRate
+        self.maxValue = maxValue
+
+        self.resetCounter()
+
+    def resetCounter(self):
+        self.counter = 0
+
+    def getNextValue(self):
+        temp = math.floor(math.sin(2 * math.pi * self.counter / self.samplingRate) * self.maxValue)
+
+        self.counter+=1
+        return temp
+
+wave = Wave(1)
+i = 0
+while i < 44100:
+    print(wave.getNextValue())
+    i+=1
