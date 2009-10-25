8f8f6bb7a4a580f85beac0f8f61dc1714c3b9ef9
Edmundo Carmona <eantoranz@gmail.com>
2009-10-25 00:44:06 -0500

    Starting a wave that can change its frequency (still needs some work.. and testing)

--- bin/Wave.py
+++ bin/Wave.py
@@ -28,3 +28,16 @@ class Wave:
 
         self.counter+=1
         return temp
+
+class ChangingWave(Wave):
+
+    def __setFrequency(self, newFreq):
+        """ Used to change the frequency of the wave to a new frequency """
+        # let's calculate the point of the wave where it is right now
+        ticksPerWave = self.samplingRate / self.freq
+        mod = self.counter % ticksPerWave
+        oldPosition = mod * 2 * math.pi / ticksPerWave
+        # new ticks per wave TODO this can break the movement of the wave (ex: if we are going down between 0 and the bottom)
+        self.freq = newFreq
+        ticksPerWave = self.samplingRate / self.freq
+        counter = int(oldPosition * ticksPerWave / (2 * math.pi))
