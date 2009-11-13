7a9a70ea4dadf6ffe2cd9c7542f9c9e0b1bc3972
Edmundo Carmona <eantoranz@gmail.com>
2009-11-13 17:51:14 -0500

    Included a class that is a token and that can be called to show messages for the user showing line and position of the problematic word

--- bin/lilypy.py
+++ bin/lilypy.py
@@ -111,6 +111,21 @@ class MusicalNote:
     def __repr__(self):
         return self.toString()
 
+class LilypondToken:
+
+    def __init__(self, word, line, pos):
+        self.word = word
+        self.line = line
+        self.pos = pos
+
+    def toString(self):
+        return "Token: " + str(self.word) + " Line " + str(self.line) + " Pos " + str(self.pos)
+
+    def raiseException(self, message = None):
+        if message == None:
+            raise Exception("Unexpected \"" + self.word + "\" on line " + str(self.line) + " pos " +  str(self.pos))
+        else:
+            raise Exception(str(message) + " on line " + str(self.line) + " pos " + str(self.pos))
 
 class LilypondHeader:
 
@@ -120,7 +135,7 @@ class LilypondHeader:
         """
         self.title = None
 
-    def setTitle(self, title):
+    def setTitle(self, title): 
         self.title = title
 
     def toString(self):
@@ -140,26 +155,28 @@ class LilypondHeader:
         tokenIndex = openingIndex + 1
         while tokenIndex < len(tokens):
             token = tokens[tokenIndex]
-            if token != "\n":
-                if token == "\\title":
+            if token.word != "\n":
+                if token.word == "\\title":
+                    titleLine = token.line
                     # user wants to set the title
-                    if tokens[tokenIndex + 1] == "=" and tokens[tokenIndex + 2] != "\n":
+                    if tokens[tokenIndex + 1].word == "=":
                         # all that's following is the title till the line closes
                         title = ""
                         tokenIndex += 2
-                        while tokens[tokenIndex] != "\n":
+                        while tokens[tokenIndex].line == titleLine:
                             if len(title) > 0:
                                 title += " "
-                            title += tokens[tokenIndex]
+                            title += tokens[tokenIndex].word
                             tokenIndex += 1
                         # Got to the end of the line
                         self.setTitle(title)
+                        continue
                     else:
                         raise Exception("Unexpected title definition")
-                elif token == "}":
+                elif token.word == "}":
                     break
                 else:
-                    raise Exception("Unexpected token in header: " + token)
+                    token.raiseException()
             tokenIndex+=1
         if tokenIndex >= len(tokens):
             raise Exception("Unexpected end of file")
@@ -181,7 +198,7 @@ class LilypondStaff:
 
     def readNote(self, token, absolute = False):
         """ Have to return a single note from a given token """
-        note=token[0]
+        note=token.word[0]
         if note == "a":
             note=MusicalNote.NOTE_A
         elif note == "b":
@@ -197,7 +214,7 @@ class LilypondStaff:
         elif note == "g":
             note=MusicalNote.NOTE_G
         else:
-            raise Exception("Unknown note: " + token[0])
+            token.raiseException("Unknown note: " + token.word[0])
 
         # TODO have to get the alteration.... but won't do it right now
 
@@ -209,22 +226,22 @@ class LilypondStaff:
             # TODO If it's relative, there are more calculations to make to know the index
 
         charIndex=1
-        if charIndex < len(token):
-            if token[charIndex] == ',':
+        if charIndex < len(token.word):
+            if token.word[charIndex] == ',':
                 difference = -1
-            elif token[charIndex] == '\'':
+            elif token.word[charIndex] == '\'':
                 difference = 1
         if difference != 0:
-            while charIndex < len(token) and token[charIndex]:
-                if token[charIndex] not in ",\'":
+            while charIndex < len(token.word) and token.word[charIndex]:
+                if token.word[charIndex] not in ",\'":
                     break; # finished
-                if difference == -1 and token[charIndex] != ',' or difference == 1 and token[charIndex] != '\'':
-                    raise Exception("Unexpected " + token[charIndex])
+                if difference == -1 and token.word[charIndex] != ',' or difference == 1 and token.word[charIndex] != '\'':
+                    token[charIndex].raiseException()
                 index+=difference
                 charIndex+=1
 
         # Got to the end of the index.... how about the duration?
-        if charIndex >= len(token):
+        if charIndex >= len(token.word):
             # Got to the end of the note... have to use the previous duration
             return MusicalNote(note, 0, index, self.lastDuration)
         # One duration must follow
@@ -234,9 +251,9 @@ class LilypondStaff:
 
     def getStaffFromTokens(self, tokens, tokenIndex):
         """ Nothing yet """
-        if tokens[tokenIndex+1] != '=':
+        if tokens[tokenIndex+1].word != '=':
             raise Exception("Unexpected Staff definition")
-        if tokens[tokenIndex+2] != '\\relative':
+        if tokens[tokenIndex+2].word != '\\relative':
             raise Exception("Don't know how to read non-relative staffs")
         # Let's read the relative note
         self.lastReferenceNote = self.readNote(tokens[tokenIndex+3], True)
@@ -252,21 +269,24 @@ class LilypondAnalyser:
         self.staffs = []
 
     def readTokens(self, lines):
+        lineCounter = 0
         for line in lines:
+            lineCounter += 1
             token = "" # Starting a line, word is empty
+            posCounter=0
             for char in line:
+                posCounter+=1
                 if char == " " or char == "\t":
                     # space or tab
                     if len(token) > 0:
                         # there is something in the token
-                        self.tokens.append(token)
+                        self.tokens.append(LilypondToken(token, lineCounter, posCounter - len(token)))
                         token = ""
                 elif char == "\n":
                     # end of line
-                    if len(token)  > 0:
-                        self.tokens.append(token)
+                    if len(token) > 0:
+                        self.tokens.append(LilypondToken(token, lineCounter, posCounter - len(token)))
                         # Don't have to reset... it will be done when starting the next line
-                        self.tokens.append("\n")
                 else:
                     # any other character
                     token += char
@@ -282,26 +302,26 @@ class LilypondAnalyser:
         while tokenIndex < len(self.tokens):
             tokenIndex+=1
             token = self.tokens[tokenIndex]
-            if token == "\n":
+            if token.word == "\n":
                 # Doesn't matter
                 continue
-            if token == "\\header":
+            if token.word == "\\header":
                 if self.header != None:
                     raise Exception("Header had already been set")
-                if self.tokens[tokenIndex + 1] != '{':
+                if self.tokens[tokenIndex + 1].word != '{':
                     raise Exception("Header definition is wrong")
                 self.header = LilypondHeader()
                 tokenIndex = self.header.getHeaderFromTokens(self.tokens, tokenIndex + 1) # from the opening { will return the closing }
-            elif token == "\\new":
+            elif token.word == "\\new":
                 # Is it one new staff?
-                if self.tokens[tokenIndex+1] == "Staff":
+                if self.tokens[tokenIndex+1].word == "Staff":
                     # It's a new staff
                     staff = LilypondStaff()
                     # Let's read the staff
                     tokenIndex = staff.getStaffFromTokens(self.tokens, tokenIndex + 1) #Pass the opening Staff will return the closing }
                     self.staffs.append(staff)
             else:
-                raise Exception("Unexpected " + token)
+                token.raiseException()
 
 
                     
