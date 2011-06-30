6c8ed8118044efe54e7f57180114d0ad274b1a2f
Edmundo Carmona <eantoranz@gmail.com>
2011-06-29 22:06:39 -0500

    SF2: Parsing SHDR

--- bin/sf2.py
+++ bin/sf2.py
@@ -7,6 +7,12 @@ import sys
 from StringIO import StringIO
 from chunk import Chunk
 
+def strToDWord(string):
+    return ord(string[3])<<24 | ord(string[2])<<16 | ord(string[1])<<8 | ord(string[0])
+
+def strToWord(string):
+    ord(string[1])<<8 | ord(string[0])
+
 class SF2Parser:
     # this class is used to parse SF2 file contents
     
@@ -22,7 +28,7 @@ class SF2Parser:
         aChunk.close()
         
         # let's create a node from this data
-        node = SF2Node(nodeName, ckID, ckSize, ckData)
+        node = SF2Parser.createNode(nodeName, ckID, ckSize, ckData)
         
         # depending on the type of node, children could be inside of it
         SF2Parser.parseNode(node)
@@ -48,6 +54,14 @@ class SF2Parser:
             except EOFError:
                 #sys.stderr.write("Reached EOF... coming back a level\n")
                 None
+    
+    @classmethod
+    def createNode(cls, nodeName, ckID, ckSize, ckData):
+        if ckID == "shdr":
+            # sample pointers
+            return SF2ShdrNode(nodeName, ckID, ckSize, ckData)
+        else:
+            return SF2Node(nodeName, ckID, ckSize, ckData)
                 
 
 class SF2Node:
@@ -82,6 +96,40 @@ class SF2Node:
             name = self.name
         return "Node " + name + ". ID: " + self.ckID + ". Size " + str(self.ckSize)
 
+class SF2ShdrRecord:
+    # one sample record information in shdr
+
+    def __init__(self, name, start, end, loopStart, loopEnd, sampleRate, midiPitch):
+        (self.name, self.start, self.end, self.loopStart, self.loopEnd, self.sampleRate, self.midiPitch) = (name, start, end, loopStart, loopEnd, sampleRate, midiPitch)
+    
+    def __str__(self):
+        return "Sample name: <" + self.name + ">. Start: " + str(self.start) + " End: " + str(self.end) + " Loop Start: " + str(self.loopStart) + " Loop End: " + str(self.loopEnd) + " Sample Rate: " + str(self.sampleRate) + " Midi Pitch: " + str(self.midiPitch)
+
+class SF2ShdrNode(SF2Node):
+    # sample pointers
+    
+    def __init__(self, name, ckID, ckSize, ckData, parent = None):
+        SF2Node.__init__(self, name, ckID, ckSize, ckData, parent = None)
+        
+        self.records = list()
+        
+        # let's break the information of the pointers
+        # how many records are there?
+        records = self.ckSize / 46
+        for i in range(records):
+            baseIndex = i * 46
+            name = self.ckData[baseIndex:baseIndex + 20]
+            if name == "EOS":
+                # this is the last empty sample... won't add it
+                break
+            start = strToDWord(self.ckData[baseIndex+20:baseIndex+24])
+            end = strToDWord(self.ckData[baseIndex+24:baseIndex+28])
+            loopStart = strToDWord(self.ckData[baseIndex+28:baseIndex+32])
+            loopEnd = strToDWord(self.ckData[baseIndex+32:baseIndex+36])
+            sampleRate = strToDWord(self.ckData[baseIndex+36:baseIndex+40])
+            midiPitch = ord(self.ckData[baseIndex+40])
+            
+            self.records.append(SF2ShdrRecord(name, start, end, loopStart, loopEnd, sampleRate, midiPitch))
 
 if __name__ == "__main__":
     # a file name should have been provided to be processed
@@ -90,6 +138,11 @@ if __name__ == "__main__":
     sf2Tree = SF2Parser.parseFromFile(iFile)
     # let's print some information about the SF2 file
     # engineer
-    info = sf2Tree.getChild("IENG", True)
-    sys.stderr.write(info.ckData + "\n")
+    smpl = sf2Tree.getChild("smpl", True)
+    sys.stdout.write(smpl.ckData)
     
+    shdr = sf2Tree.getChild("shdr", True)
+    sys.stderr.write("There are " + str(shdr.ckSize / 46) + " samples\n")
+    
+    for record in shdr.records:
+        sys.stderr.write("SHDR record : " + str(record) + "\n")
